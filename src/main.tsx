b2a1d49caf885ad201d2851546e181b5b0929db4
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-04-03 09:24:57 +0530

    feat: route setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<HomePage />}></Route>
+          <Route path="/login" element={<HomePage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/watch-list" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
